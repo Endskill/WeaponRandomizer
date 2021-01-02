7b72a57a24a2d03e1483f68bc1cbb497dfe088f5
Endskill <endskillgaming@gmail.com>
2021-01-02 15:31:40 +0100

    Trying to get it running with the Latest BepInEx Version

--- WeaponRandomizer/BepInExLoader.cs
+++ WeaponRandomizer/BepInExLoader.cs
@@ -9,7 +9,6 @@ using System.Text;
 using System.Threading.Tasks;
 using UnhollowerRuntimeLib;
 using UnityEngine;
-using WeaponRandomizer.Overlay;
 
 namespace WeaponRandomizer
 {
@@ -20,28 +19,31 @@ namespace WeaponRandomizer
          MODNAME = "WeaponRandomizer",
          AUTHOR = "Endskill",
          GUID = AUTHOR + "." + MODNAME,
-         VERSION = "1.1";
+         VERSION = "1.1.1";
 
         public override void Load()
         {
-            ClassInjector.RegisterTypeInIl2Cpp<InputHandler>();
+            ClassInjector.RegisterTypeInIl2Cpp<Entry>();
+            //GameObject gameObject = new GameObject("CheatMenu");
+            //gameObject.AddComponent<Entry>();
+            //UnityEngine.Object.DontDestroyOnLoad((UnityEngine.Object)gameObject);
 
-            var harmony = new Harmony("WeaponRandomizer");
-
+            Console.WriteLine("Patching All ...");
+            var harmony = new Harmony(GUID);
             harmony.PatchAll();
+            Console.WriteLine("Patched");
         }
-    }
-
-    [HarmonyPatch(typeof(CM_PageIntro), "Setup")]
-    public class PrepareInjection
-    {
-        public static GameObject _obj;
-        [HarmonyPostfix]
-        public static void PostFix()
+        [HarmonyPatch(typeof(CM_PageIntro), "Setup")]
+        public class PrepareInjection
         {
-            var _obj = new GameObject();
-            _obj.AddComponent<InputHandler>();
-            UnityEngine.Object.DontDestroyOnLoad(_obj);
+            public static GameObject _obj;
+            [HarmonyPostfix]
+            public static void PostFix()
+            {
+                var _obj = new GameObject();
+                _obj.AddComponent<Entry>();
+                UnityEngine.Object.DontDestroyOnLoad(_obj);
+            }
         }
     }
 }
---
+++ WeaponRandomizer/Entry.cs
@@ -0,0 +1,118 @@
+﻿using Player;
+using System;
+using UnityEngine;
+using WeaponRandomizer.WeaponRandomizer;
+
+namespace WeaponRandomizer
+{
+    public class Entry : MonoBehaviour
+    {
+        private WeaponRandomizer.WeaponRandomizer _weaponRandomizer;
+        private string _normalName;
+
+        private DateTime _nextPeriodInterval;
+
+        private bool _isActivated = false;
+        private bool _wasInLevel;
+
+        public Entry(IntPtr intPtr) : base(intPtr)
+        {
+            _normalName = SteamManager.LocalPlayerName;
+            _weaponRandomizer = new WeaponRandomizer.WeaponRandomizer();
+        }
+
+        public string NormalName
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_normalName))
+                {
+                    _normalName = SteamManager.LocalPlayerName;
+                }
+
+                return _normalName;
+            }
+        }
+
+        public bool WasInLevel
+        {
+            get => _wasInLevel;
+            set
+            {
+                if (!value)
+                {
+                    _weaponRandomizer.NewExpeditionStarted();
+                }
+
+                _wasInLevel = value;
+            }
+        }
+
+        public void Update()
+        {
+            if ((byte)GameStateManager.CurrentStateName < 5)
+            {
+                if (Input.GetKeyDown(KeyCode.R) || Input.GetKeyDown(KeyCode.N) || Input.GetKeyDown(KeyCode.G))
+                {
+                    if (Input.GetKey(KeyCode.R) && Input.GetKey(KeyCode.N) && Input.GetKey(KeyCode.G))
+                    {
+                        //To prevent Activating / Deactivating it while in Game
+                        if (PlayerManager.PlayerAgentsInLevel.Count == 0)
+                        {
+                            Console.WriteLine($"WeaponRandomizer = {(!_isActivated ? "Turned ON" : "Turned OFF")}");
+                            if (_isActivated)
+                            {
+                                ModIdentifier.ModdingName = _normalName;
+                                _isActivated = false;
+                            }
+                            else
+                            {
+                                _isActivated = true;
+                                ModIdentifier.ModdingName = $"[Modded] {_normalName}";
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                WeaponRandomizer.WeaponRandomizer.LocalInventory = UnityEngine.Object.FindObjectOfType<PlayerInventoryLocal>();
+                _weaponRandomizer.RandomizeCurrentWeapons();
+            }
+
+            //Some Timer imitation, because the Normal System.Timer thing does not work
+            if (_nextPeriodInterval < DateTime.Now)
+            {
+                if (_isActivated && GameStateManager.CurrentStateName is eGameStateName.InLevel)
+                {
+                    if (PlayerManager.PlayerAgentsInLevel.Count != 0)
+                    {
+                        if (!WasInLevel)
+                        {
+                            WeaponRandomizer.WeaponRandomizer.LocalInventory = UnityEngine.Object.FindObjectOfType<PlayerInventoryLocal>();
+                            WasInLevel = true;
+                        }
+
+                        switch (_weaponRandomizer.ZoneSwitched())
+                        {
+                            case ActionRequired.NewZoneEntered:
+                                _weaponRandomizer.RandomizeCurrentWeapons();
+                                break;
+                            case ActionRequired.ExploredZoneEntered:
+                                _weaponRandomizer.NewZoneEntered();
+                                break;
+                        }
+                    }
+                }
+                else if (WasInLevel)
+                {
+                    WeaponRandomizer.WeaponRandomizer.LocalInventory = null;
+                    WasInLevel = false;
+                }
+
+                _nextPeriodInterval = DateTime.Now.AddMilliseconds(500);
+            }
+        }
+    }
+}
